edfa54f06ce01bf27ae99fdc06e7ebd4378b7370
Rone Filho <rone_filho@hotmail.com>
2019-02-04 13:14:05 -0200

    Adding client-side validation

--- CRISPR.Anotations/wwwroot/js/site.js
+++ CRISPR.Anotations/wwwroot/js/site.js
@@ -5,3 +5,17 @@ lang.dynamic('en', 'js/langpack/en.json');
 lang.init({
     defaultLang: 'pt'
 });
+
+window.onload = () => {
+
+    let form = document.getElementById("form");
+
+    form.addEventListener('submit', (event) => {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+    });
+
+};
